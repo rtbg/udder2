f29b309ef888e757e2beac68ca2fce812d3b9aa7
Robertas Bobertas <r.gircys@gmail.com>
2019-12-31 12:20:16 -0500

    Initial Commit

---
+++ udder2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  udder2
+//
+//  Created by Robertas Gircys on 12/31/19.
+//  Copyright © 2019 Robertas Gircys. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
